5ff8f0dbd0041a44a8bbf62fb8b1b755edaa0a4d
Vincent Acanfora <brodino96@gmail.com>
2025-07-26 22:08:47 +0200

    feat: Config can now be reloaded

--- src/main/java/net/brodino/dimensionaldamage/DimensionalDamage.java
+++ src/main/java/net/brodino/dimensionaldamage/DimensionalDamage.java
@@ -2,6 +2,9 @@ package net.brodino.dimensionaldamage;
 
 import net.brodino.dimensionaldamage.Config;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +16,17 @@ public class DimensionalDamage implements ModInitializer {
     @Override
     public void onInitialize() {
         LOGGER.info("Initializing DimensionalDamage - Players will not take damage in the overworld");
+
+        CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(CommandManager.literal("dimensionaldamage")
+                    .requires(source -> source.hasPermissionLevel(2))
+                    .executes(context -> {
+                        LOGGER.info("Reloading Dimensional Damage's configs!");
+                        CONFIG.load();
+                        context.getSource().getPlayer().sendMessage(Text.literal("Config reloaded!"), false);
+                        return 1;
+                    })
+            );
+        }));
     }
 }
